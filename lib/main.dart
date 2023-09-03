6bac9ee242315e5f22f8d22a43ef33cadbc13b8c
benjiki <88052682+benjiki@users.noreply.github.com>
2023-09-03 17:05:25 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:meya/pages/CategoryPage.dart';
 import 'package:meya/pages/HomePage.dart';
 import 'package:meya/pages/profilePage.dart';
-
+import 'package:meya/pages/Allpages.dart;
 void main() {
   runApp(Myapp());
 }
